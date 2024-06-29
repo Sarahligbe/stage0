1653f53fbf545dad2a7b7c72535b0117a5127d2e
Sarahligbe <sarahligbe12@gmail.com>
2024-06-29 09:17:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const myInfo = {
+    name: "Sarah Aligbe",
+    slackUsername: "Sarah Aligbe",
+    email: "sarahligbe12@gmail.com"
+};
+
+document.getElementById("name").textContent = myInfo.name;
+document.getElementById("slack").textContent = myInfo.slackUsername;
+document.getElementById("email").textContent = myInfo.email;
